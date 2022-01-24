bba6298e194fc95cbb0af85812bdbbd74e4956f3
Dustin Welch <dustinwwelch1@gmail.com>
2022-01-23 22:24:13 -0600

    fixed bug with category selector

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+const categories = ["fruit", "vegetable", "dairy", "baked goods"];
+
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
   res.render("products/index", {
@@ -31,7 +33,7 @@ app.get("/products", async (req, res) => {
 });
 
 app.get("/products/new", (req, res) => {
-  res.render("products/new");
+  res.render("products/new", { categories });
 });
 
 app.post("/products", async (req, res) => {
@@ -50,7 +52,7 @@ app.get("/products/:id", async (req, res) => {
 app.get("/products/:id/edit", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
-  res.render("products/edit", { product });
+  res.render("products/edit", { product, categories });
 });
 
 app.put("/products/:id", async (req, res) => {
