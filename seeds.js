a6b991ea4a592fdabc18f2497a4945d5840c1383
Dustin Welch <dustinwwelch1@gmail.com>
2022-01-23 19:49:14 -0600

    created and tested first product from Product model

--- seeds.js
+++ seeds.js
@@ -1,8 +1,4 @@
-const express = require("express");
-const app = express();
-const path = require("path");
 const mongoose = require("mongoose");
-
 const Product = require("./models/product");
 
 mongoose
@@ -15,13 +11,15 @@ mongoose
     console.log(err);
   });
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-app.get("/dog", (req, res) => {
-  res.send("WOOF!");
+const p = new Product({
+  name: "Ruby Grapefruit",
+  price: 1.99,
+  category: "fruit",
 });
-
-app.listen(3000, () => {
-  console.log("APP IS LISTENING ON PORT 3000");
-});
+p.save()
+  .then((p) => {
+    console.log(p);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
