a1599045bf186005ef4d7631421c8691ff94b643
Dustin Welch <dustinwwelch1@gmail.com>
2022-01-23 19:31:53 -0600

    express server set up

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/dog", (req, res) => {
+  res.send("WOOF!");
+});
+
+app.listen(3000, () => {
+  console.log("APP IS LISTENING ON PORT 3000");
+});
